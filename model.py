68e85b7a9596b64277f4402ae56b0c1e07133530
lgvaz <lucasgouvaz@gmail.com>
2017-08-03 19:08:04 -0300

    Moved model to a class

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import numpy as np
-import tensorflow as tf
 import keras
 from keras.models import Model
 from keras.layers import Lambda, Input
@@ -7,53 +6,54 @@ from keras.layers import Dense, Flatten
 from keras.layers import Conv2D
 from keras.optimizers import Adam, RMSprop, SGD
 from keras import backend as K
+from utils import *
 
 NUM_ACTIONS = 2
 
-def mask_loss(actions):
-    def loss(y_true, y_pred):
-        onehot_actions = K.one_hot(actions, NUM_ACTIONS)
-        errors = huber_loss(y_true, y_pred)
-        return K.mean(onehot_actions * errors)
-        # return keras.losses.mean_squared_error(y_true, y_pred)
+class DQN:
+    ''' Network model from DeepMind '''
+    def __init__(self, state_shape, num_actions, learning_rate):
+        self.state_shape = state_shape
+        self.num_actions = num_actions
+        self.lr = learning_rate
 
-    return loss
+        self.model = self._build_model()
 
-def huber_loss(y_true, y_pred, delta=1):
-    '''
-    Hubber loss is less sensitive to outliers
-    https://en.wikipedia.org/wiki/Huber_loss
-    '''
-    error = y_true - y_pred
-    condition = K.abs(error) <= delta
-    squared_error =  0.5 * K.square(error)
-    linear_error = delta * (K.abs(error) - 0.5 * delta)
-    return tf.where(condition, squared_error, linear_error)
+    def _build_model(self):
+        # Model inputs
+        states = Input(shape=(self.state_shape))
+        actions = Input(shape=(1,), dtype='int32')
 
-# class DQN:
-#     ''' Network model from DeepMind '''
-#     def __init__(self):
-# Model inputs
-actions = Input(shape=(1,), dtype='int32')
-states = Input(shape=(4,))
+        # Model architecture 
+        output = Dense(self.num_actions)(states)
 
-# Model architecture 
-output = Dense(NUM_ACTIONS)(states)
+        model = Model(inputs=[states, actions], outputs=output, name='predictions')
+        model.compile(optimizer=SGD(self.lr), loss=mask_loss(actions, self.num_actions))
 
-model = Model(inputs=[states, actions], outputs=output, name='predictions')
-model.compile(optimizer=SGD(1e-1), loss=mask_loss(actions=actions))
+        return model
 
+    def predict(self, states):
+        fake_actions = np.zeros(len(states))
+        return self.model.predict([states, fake_actions])
+
+    def fit(self, states, actions, labels, epochs=1):
+        self.model.fit([states, actions], labels, epochs=epochs)
+    
 fake_inputs = np.random.random((1, 4))
 fake_labels = np.random.random((1, 1))
-fake_labels = np.array([(100)])
+fake_labels = np.array([(5)])
 # fake_actions = np.random.randint(0, 1, 2, np.int32)
 fake_actions = np.array([0])
 
-old_preds = model.predict([fake_inputs, fake_actions])
+model = DQN(state_shape=(4,), num_actions=2, learning_rate=1e-1)
 
-h = model.fit([fake_inputs, fake_actions], fake_labels, epochs=200)
+old_preds = model.predict(fake_inputs)
 
-new_preds = model.predict([fake_inputs, fake_actions])
+model.fit(fake_inputs, fake_actions, fake_labels, epochs=200)
+
+# h = model.model.fit([fake_inputs, fake_actions], fake_labels, epochs=200)
+
+new_preds = model.predict(fake_inputs)
 print('Original: {}'.format(fake_labels))
 print('Old preds: {}'.format(old_preds))
 print('New preds {}'.format(new_preds))
