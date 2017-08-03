dcabc75649a0b2c55b0629f4ba4f3a21b07965be
lgvaz <lucasgouvaz@gmail.com>
2017-08-03 17:02:01 -0300

    Added hubber loss

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 import keras
 from keras.models import Model
 from keras.layers import Lambda, Input
@@ -12,21 +13,34 @@ NUM_ACTIONS = 2
 def mask_loss(actions):
     def loss(y_true, y_pred):
         onehot_actions = K.one_hot(actions, NUM_ACTIONS)
-        r = y_true - y_pred
-        return K.mean(onehot_actions * K.square(r))
+        errors = huber_loss(y_true, y_pred)
+        return K.mean(onehot_actions * errors)
         # return keras.losses.mean_squared_error(y_true, y_pred)
 
     return loss
 
+def huber_loss(y_true, y_pred, delta=1):
+    '''
+    Hubber loss is less sensitive to outliers
+    https://en.wikipedia.org/wiki/Huber_loss
+    '''
+    error = y_true - y_pred
+    condition = K.abs(error) <= delta
+    squared_error =  0.5 * K.square(error)
+    linear_error = delta * (K.abs(error) - 0.5 * delta)
+    return tf.where(condition, squared_error, linear_error)
+
 # class DQN:
 #     ''' Network model from DeepMind '''
 #     def __init__(self):
+# Model inputs
 actions = Input(shape=(1,), dtype='int32')
-inputs = Input(shape=(4,))
-x = Dense(16)(inputs)
-output = Dense(NUM_ACTIONS)(inputs)
+states = Input(shape=(4,))
 
-model = Model(inputs=[inputs, actions], outputs=output, name='predictions')
+# Model architecture 
+output = Dense(NUM_ACTIONS)(states)
+
+model = Model(inputs=[states, actions], outputs=output, name='predictions')
 model.compile(optimizer=SGD(1e-1), loss=mask_loss(actions=actions))
 
 fake_inputs = np.random.random((1, 4))
