682b8f4270384249f1629979d1691d4d50ea784b
lgvaz <lucasgouvaz@gmail.com>
2017-08-04 13:48:26 -0300

    Added a model for simple envs

--- model.py
+++ model.py
@@ -11,28 +11,52 @@ from utils import *
 NUM_ACTIONS = 2
 
 class DQN:
-    ''' Network model from DeepMind '''
     def __init__(self, state_shape, num_actions, learning_rate):
         self.state_shape = state_shape
         self.num_actions = num_actions
         self.lr = learning_rate
 
-        self.model = self._build_model()
+        if len(state_shape) == 3:
+            self.model = self._build_deepmind_model()
+        elif len(state_shape) == 1:
+            self.model = self._build_dense_model()
+        else:
+            raise ValueError('state_shape not supported')
 
-    def _build_model(self):
+    def _build_deepmind_model(self):
+        ''' Network model from DeepMind '''
         # Model inputs
         states = Input(shape=(self.state_shape))
         actions = Input(shape=(1,), dtype='int32')
 
-        # # Model architecture 
-        # net = Conv2D(32, (8, 8), strides=(4, 4), activation='relu')(states)
-        # net = Conv2D(64, (4, 4), strides=(2, 2), activation='relu')(net)
-        # net = Conv2D(64, (3, 3), strides=(1, 1), activation='relu')(net)
-
+        # Model architecture
         net = states
+        # Convolutional layers
+        net = Conv2D(32, (8, 8), strides=(4, 4), activation='relu')(net)
+        net = Conv2D(64, (4, 4), strides=(2, 2), activation='relu')(net)
+        net = Conv2D(64, (3, 3), strides=(1, 1), activation='relu')(net)
         net = Flatten()(net)
-        # net = Dense(512, activation='relu')(net)
 
+        # Dense layers
+        net = Dense(512, activation='relu')(net)
+
+        output = Dense(self.num_actions)(net)
+
+        model = Model(inputs=[states, actions], outputs=output, name='predictions')
+        model.compile(optimizer=Adam(self.lr), loss=mask_loss(actions, self.num_actions))
+
+        return model
+
+    def _build_dense_model(self):
+        ''' Simple fully connected model '''
+        # Model inputs
+        states = Input(shape=(self.state_shape))
+        actions = Input(shape=(1,), dtype='int32')
+
+        # Model architecture
+        net = states
+        net = Dense(64, activation='relu')(net)
+        # net = Dense(64, activation='relu')(net)
         output = Dense(self.num_actions)(net)
 
         model = Model(inputs=[states, actions], outputs=output, name='predictions')
--- test_model.py
+++ test_model.py
@@ -5,16 +5,18 @@ STATE_SHAPE = [84, 84, 1]
 NUM_ACTIONS = 3
 LEARNING_RATE = 1e-3
 
+# STATE_SHAPE = [4]
+# NUM_ACTIONS = 3
+# # A higher learning rate can be used for simple envs
+# LEARNING_RATE = 1e-2
+
 for i_action in range(NUM_ACTIONS):
 
     model = DQN(STATE_SHAPE, NUM_ACTIONS, LEARNING_RATE)
 
     fake_inputs = np.random.random([NUM_ACTIONS] + STATE_SHAPE)
     fake_labels = np.arange(1, NUM_ACTIONS + 1) * 2
-    # fake_actions = np.arange(NUM_ACTIONS)
-    # fake_labels = np.array([(5)])
     fake_actions = np.array([i_action] * NUM_ACTIONS)
-
 
     old_preds = model.predict(fake_inputs)
     model.fit(fake_inputs, fake_actions, fake_labels, epochs=100, verbose=0)
