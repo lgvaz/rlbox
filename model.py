f44cf8f8da7b42eb1623f9b1bafa48142356eea9
lgvaz <lucasgouvaz@gmail.com>
2017-08-16 00:37:04 -0300

    Create target update operation

--- model.py
+++ model.py
@@ -43,8 +43,8 @@ class DQN:
             states_t_float = tf.cast(self.states_t, tf.float32) / 255.
             states_tp1_float = tf.cast(self.states_tp1, tf.float32) / 255.
 
-            self.q_values = self._build_deepmind_model(states_t_float)
-            self.q_target = self._build_deepmind_model(states_tp1_float)
+            self.q_values = self._build_deepmind_model(states_t_float, 'online')
+            self.q_target = self._build_deepmind_model(states_tp1_float, 'target')
 
         elif len(state_shape) == 1:
             print('shape1')
@@ -58,8 +58,8 @@ class DQN:
                 shape=[None] + list(self.state_shape),
                 dtype=tf.float32
             )
-            self.q_values = self._build_dense_model(self.states_t)
-            self.q_target = self._build_dense_model(self.states_tp1)
+            self.q_values = self._build_dense_model(self.states_t, 'online')
+            self.q_target = self._build_dense_model(self.states_tp1, 'target')
 
         else:
             raise ValueError('state_shape not supported')
@@ -68,30 +68,34 @@ class DQN:
         # TODO: Remove hardcoded gamma
         self.training_op = self._build_optimization(learning_rate, 0.99)
 
-    def _build_deepmind_model(self, states):
+        self.update_target_op = self._build_target_update_op()
+
+    def _build_deepmind_model(self, states, scope):
         ''' Network model from DeepMind '''
-        # Model architecture
-        net = states
-        # Convolutional layers
-        net = tf.layers.conv2d(net, 32, (8, 8), strides=(4, 4), activation=tf.nn.relu)
-        net = tf.layers.conv2d(net, 64, (4, 4), strides=(2, 2), activation=tf.nn.relu)
-        net = tf.layers.conv2d(net, 32, (3, 3), strides=(1, 1), activation=tf.nn.relu)
-        net = tf.contrib.layers.flatten(net)
+        with tf.variable_scope(scope):
+            # Model architecture
+            net = states
+            # Convolutional layers
+            net = tf.layers.conv2d(net, 32, (8, 8), strides=(4, 4), activation=tf.nn.relu)
+            net = tf.layers.conv2d(net, 64, (4, 4), strides=(2, 2), activation=tf.nn.relu)
+            net = tf.layers.conv2d(net, 32, (3, 3), strides=(1, 1), activation=tf.nn.relu)
+            net = tf.contrib.layers.flatten(net)
 
-        # Dense layers
-        net = tf.layers.dense(net, 512, activation=tf.nn.relu)
-        output = tf.layers.dense(net, self.num_actions)
+            # Dense layers
+            net = tf.layers.dense(net, 512, activation=tf.nn.relu)
+            output = tf.layers.dense(net, self.num_actions)
 
-        return output
+            return output
 
-    def _build_dense_model(self, states):
+    def _build_dense_model(self, states, scope):
         ''' Simple fully connected model '''
-        # Model architecture
-        net = states
-        net = tf.layers.dense(net, 512, activation=tf.nn.relu)
-        output = tf.layers.dense(net, self.num_actions)
+        with tf.variable_scope(scope):
+            # Model architecture
+            net = states
+            net = tf.layers.dense(net, 512, activation=tf.nn.relu)
+            output = tf.layers.dense(net, self.num_actions)
 
-        return output
+            return output
 
     def _build_optimization(self, learning_rate, gamma):
         # Choose only the q values for selected actions
@@ -112,8 +116,21 @@ class DQN:
 
         return training_op
 
+    def _build_target_update_op(self, alpha=1):
+        # Get variables within defined scope
+        online_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'online')
+        target_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'target')
+        # Create operations that copy the variables
+        op_holder = [target_var.assign(alpha * online_var + (1 - alpha) * target_var)
+                    for online_var, target_var in zip(online_vars, target_vars)]
+
+        return op_holder
+
     def predict(self, sess, states):
         return sess.run(self.q_values, feed_dict={self.states_t: states})
+
+    def target_predict(self, sess, states):
+        return sess.run(self.q_target, feed_dict={self.states_tp1: states})
 
     def fit(self, sess, states_t, states_tp1, actions, rewards, dones):
         feed_dict = {
@@ -125,20 +142,5 @@ class DQN:
         }
         sess.run(self.training_op, feed_dict=feed_dict)
 
-    # def fit(self, states, actions, labels):
-    #     # self.model.fit([states, actions], labels, batch_size=batch_size, epochs=epochs, verbose=verbose)
-    #     loss = self.model.train_on_batch([states, actions], labels)
-    #     return loss
-
-    # def target_predict(self, states):
-    #     fake_actions = np.zeros(len(states))
-    #     return self.target.predict([states, fake_actions])
-
-    # def target_update(self):
-    #     self.target.set_weights(self.model.get_weights())
-
-    # def load_weights(self, weights_file):
-    #     self.model.load_weights(weights_file)
-    #     self.target.load_weights(weights_file)
-
-
+    def update_target_net(self, sess):
+        sess.run(self.update_target_op)
--- test_model.py
+++ test_model.py
@@ -2,6 +2,9 @@ import numpy as np
 import tensorflow as tf
 from model import DQN
 
+
+# TODO: Change print statements to asserts
+# TODO: Create fake states for imgs (need to be uint8)
 # STATE_SHAPE = [84, 84, 1]
 # NUM_ACTIONS = 3
 # LEARNING_RATE = 1e-3
@@ -11,25 +14,46 @@ NUM_ACTIONS = 3
 # A higher learning rate can be used for simple envs
 LEARNING_RATE = 1e-2
 
-model = DQN(STATE_SHAPE, NUM_ACTIONS, LEARNING_RATE)
 
 fake_states = np.random.random([3] + STATE_SHAPE)
 fake_target_states = np.random.random([3] + STATE_SHAPE)
-fake_actions = np.array([0, 0, 0])
 fake_rewards = np.array([100, 100, 100])
 fake_dones = np.array([1, 1, 1])
 
-sess = tf.InteractiveSession()
-sess.run(tf.global_variables_initializer())
+print('Testing action optimization process')
+for i_action in range(NUM_ACTIONS):
+    fake_actions = np.array(3 * [i_action])
 
-old_preds = model.predict(sess, fake_states)
-print('Old predictions:\n', old_preds)
-for _ in range(100):
-    model.fit(sess,
-            fake_states,
-            fake_target_states,
-            fake_actions,
-            fake_rewards,
-            fake_dones)
-new_preds = model.predict(sess, fake_states)
-print('New predictions:\n', new_preds)
+    tf.reset_default_graph()
+    model = DQN(STATE_SHAPE, NUM_ACTIONS, LEARNING_RATE)
+
+    print('Optimizing for action', i_action)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        old_preds = model.predict(sess, fake_states)
+        print('Old predictions:\n', old_preds)
+        for _ in range(100):
+            model.fit(sess,
+                      fake_states,
+                      fake_target_states,
+                      fake_actions,
+                      fake_rewards,
+                      fake_dones)
+        new_preds = model.predict(sess, fake_states)
+        print('New predictions:\n', new_preds)
+
+print('Testing target update process')
+tf.reset_default_graph()
+model = DQN(STATE_SHAPE, NUM_ACTIONS, LEARNING_RATE)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    online_preds = model.predict(sess, fake_states)
+    old_target_preds = model.target_predict(sess, fake_states)
+    model.update_target_net(sess)
+    new_target_preds = model.target_predict(sess, fake_states)
+
+    print('Online predictions:\n', online_preds)
+    print('Old target predictions:\n', old_target_preds)
+    print('New target predictions:\n', new_target_preds)
