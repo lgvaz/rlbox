89309de2c288d408afb7168f51bc1f8bd69a3408
lgvaz <lucasgouvaz@gmail.com>
2017-08-25 14:23:52 -0300

    Atari wrapper basic functions

--- atari_wrapper.py
+++ atari_wrapper.py
@@ -1,28 +1,45 @@
 import gym
+from gym import Wrapper
 import cv2
 import numpy as np
 
-class AtariWrapper:
-    def __init__(self, env_name):
-        self.env = gym.make(env_name)
+class AtariWrapper(Wrapper):
+    def __init__(self, env, history_length):
+        super(AtariWrapper, self).__init__(env)
+        self.observation_space = gym.spaces.Box(0, 255, shape=(84, 84, history_length))
+        self.history_length = history_length
+        self.history_buffer = None
 
     def _preprocess_frame(self, frame):
         img = np.reshape(frame, [210, 160, 3]).astype(np.float32)
-        img *= [0.299, 0.587, 0.114]
-        resized_screen = cv2.resize(img, (84, 110),  interpolation=cv2.INTER_LINEAR)
+        img = np.dot(img, [0.299, 0.587, 0.114])
+        resized_screen = cv2.resize(img, (84, 110), interpolation=cv2.INTER_LINEAR)
         x_t = resized_screen[18:102, :]
         x_t = np.reshape(x_t, [84, 84, 1])
+
         return x_t.astype(np.uint8)
 
     def step(self, action):
         state, reward, done, info = self.env.step(action)
+        state = self._preprocess_frame(state)
 
-        return self._preprocess_frame(state), reward, done, info
+        self.history_buffer = np.concatenate((self.history_buffer[..., 1:], state), axis=-1)
+
+        return self.history_buffer, reward, done, info
 
     def reset(self):
-        state = env.reset()
+        state = self.env.reset()
+        state = self._noops()
+        state = self._preprocess_frame(state)
 
-        return self._preprocess_frame(state)
+        self.history_buffer = np.zeros((84, 84, self.history_length - 1), dtype=np.uint8)
+        self.history_buffer = np.concatenate((self.history_buffer, state), axis=-1)
 
+        return self.history_buffer
 
-env = AtariWrapper('Pong-v0')
+    def _noops(self, num_noops=20):
+        ''' Do nothing for num_oops frames '''
+        for _ in range(num_noops):
+            state, _, _, _ = self.env.step(0)
+
+        return state
