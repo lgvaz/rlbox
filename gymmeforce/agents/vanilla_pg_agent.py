025a6eea42927cd9ad9645ae7214fdc0c847e550
lgvaz <lucasgouvaz@gmail.com>
2017-10-08 22:43:07 -0300

    Check for termination and some logs

--- gymmeforce/agents/vanilla_pg_agent.py
+++ gymmeforce/agents/vanilla_pg_agent.py
@@ -2,6 +2,7 @@ import os
 import gym
 import numpy as np
 import tensorflow as tf
+import itertools
 from gymmeforce.agents import BatchAgent
 from gymmeforce.models import VanillaPGModel
 
@@ -20,24 +21,35 @@ class VanillaPGAgent(BatchAgent):
     def select_action(self, state):
         return self.model.select_action(self.sess, state)
 
-    def train(self):
+    def train(self, max_iters=-1, max_episodes=-1, max_steps=-1):
         self._maybe_create_tf_sess()
-        env = gym.make(self.env_config['env_name'])
+        # env = gym.make(self.env_config['env_name'])
         # env._max_episode_steps = 2000
-        # monitored_env, env = self._create_env(os.path.join(self.log_dir, 'videos/train'))
+        monitored_env, env = self._create_env(os.path.join(self.log_dir, 'videos/train'))
 
-        for i_iter in range(20):
+        i_step = 0
+        for i_iter in itertools.count():
             # Generate policy rollouts
             trajectories = self.generate_batch(env, 1000)
             states = np.concatenate([trajectory['states'] for trajectory in trajectories])
             actions = np.concatenate([trajectory['actions'] for trajectory in trajectories])
+            rewards = np.concatenate([trajectory['rewards'] for trajectory in trajectories])
             returns = np.concatenate([trajectory['returns'] for trajectory in trajectories])
             # Train
             # returns = (returns - np.mean(returns)) / (np.std(returns) + 1e-7)
             self.model.train(self.sess, states, actions, returns,
                              policy_learning_rate=1e-4, vf_learning_rate=5e-3, num_epochs=10, logger=self.logger)
 
-            # print(np.mean(monitored_env.get_episode_rewards()[-200:]))
-            mean_rewards = (np.sum(np.concatenate([traj['rewards'] for traj in trajectories])) / len(trajectories))
-            self.logger.add_log('Rewards', mean_rewards)
-            self.logger.log('Iteration {}'.format(i_iter))
+            # Logs
+            ep_rewards = monitored_env.get_episode_rewards()
+            i_episode = len(ep_rewards)
+            num_episodes = len(trajectories)
+            i_step += len(rewards)
+            self.logger.add_log('Rewards', np.mean(ep_rewards[-50:]))
+            self.logger.log('Iter: {} | Episode: {} | Step: {}'.format(i_iter, i_episode, i_step))
+
+            # Check for termination
+            if (i_iter // max_iters >= 1
+                or i_episode // max_episodes >= 1
+                or i_step // max_steps >= 1):
+                break
