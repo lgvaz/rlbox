2f79ee1edb6043bf647f26465ee27f812164140c
lgvaz <lucasgouvaz@gmail.com>
2017-10-10 23:01:13 -0300

    Old style placeholder config

--- gymmeforce/models/vanilla_pg_model2.py
+++ gymmeforce/models/vanilla_pg_model2.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-from collections import namedtuple
 from gymmeforce.models import BaseModel
 # TODO: Maybe separete dir for graphs?
 from gymmeforce.models.policy_graphs import dense_policy_graph
@@ -8,17 +7,19 @@ from gymmeforce.common.distributions import CategoricalDist, DiagGaussianDist
 from gymmeforce.common.policy import Policy
 
 class VanillaPGModel(BaseModel):
-    def __init__(self, env_config, log_dir, policy_graph=dense_policy_graph):
+    def __init__(self, env_config, log_dir, entropy_coef=0., policy_graph=dense_policy_graph):
         super().__init__(env_config, log_dir)
+        self.entropy_coef = entropy_coef
 
-        self.config['placeholders']['states'] = [[None] + list(env_config['state_shape']),
-                                                 env_config['input_type']]
-        self.config['placeholders']['actions'] = [[None], tf.int32]
-        self.config['placeholders']['advantages'] = [[None], tf.float32]
-        self.config['placeholders']['vf_targets'] = [[None], tf.float32]
-        self.config['placeholders']['learning_rate'] = [[], tf.float32]
+        placeholders_config = {
+            'states': [[None] + list(env_config['state_shape']), env_config['input_type']],
+            'actions': [[None], tf.int32],
+            'advantages': [[None], tf.float32],
+            'vf_targets': [[None], tf.float32],
+            'learning_rate': [[], tf.float32]
+        }
 
-        self._create_placeholders(self.config['placeholders'])
+        self._create_placeholders(placeholders_config)
         self.policy = self._create_policy(self.placeholders['states'],
                                           self.placeholders['actions'],
                                           policy_graph)
@@ -27,7 +28,7 @@ class VanillaPGModel(BaseModel):
 
     def _add_losses(self):
         ''' This method should be changed to add more losses'''
-        self._pg_loss(self.policy, self.placeholders['advantages'])
+        self._pg_loss(self.policy, self.placeholders['advantages'], self.entropy_coef)
 
     def _pg_loss(self, policy, advantages, entropy_coef=0.1):
         pg_loss = -tf.reduce_mean(policy.logprob_sy * advantages)
@@ -36,18 +37,6 @@ class VanillaPGModel(BaseModel):
 
     def _create_policy(self, states_ph, actions_ph, policy_graph):
         policy = Policy(self.env_config, states_ph, actions_ph, policy_graph)
-
-        # logits = policy_graph(states_ph, self.env_config)
-
-        # policy = namedtuple('Policy', 'dist sample_action')
-        # policy.dist = CategoricalDist(logits)
-        # policy.logprob_sy = policy.dist.selected_logprob(actions_ph)
-        # policy.sample_action_sy = policy.dist.sample()
-        # policy.sample_action = lambda sess, states: sess.run(policy.sample_action_sy,
-        #                                                      feed_dict={states_ph: states})
-        # policy.entropy_sy = policy.dist.entropy()
-        # policy.entropy = lambda sess, states: sess.run(policy.entropy_sy,
-        #                                                feed_dict={states_ph: states})
 
         return policy
 
