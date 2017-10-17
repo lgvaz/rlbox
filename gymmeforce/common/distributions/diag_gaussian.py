5e3a3c63cbdc373becf8916d73785124cd28d0a5
lgvaz <lucasgouvaz@gmail.com>
2017-10-17 11:52:56 -0200

    Same logprob and kl_divergence as openai baseline

--- gymmeforce/common/distributions/diag_gaussian.py
+++ gymmeforce/common/distributions/diag_gaussian.py
@@ -6,21 +6,21 @@ class DiagGaussianDist:
     def __init__(self, mean_and_logstd, low_bound=None, high_bound=None):
         self.mean, self.logstd = mean_and_logstd
         self.std = tf.exp(self.logstd)
-        self.num_actions = tf.shape(self.mean)[1]
+        self.num_actions = tf.to_float(tf.shape(self.mean)[-1])
         self.low_bound = low_bound
         self.high_bound = high_bound
 
     def sample(self):
         sample_action = self.mean + self.std * tf.random_normal(tf.shape(self.mean))
-        if self.low_bound is not None and self.high_bound is not None:
-            sample_action = tf.clip_by_value(sample_action, self.low_bound, self.high_bound)
+        # if self.low_bound is not None and self.high_bound is not None:
+        #     sample_action = tf.clip_by_value(sample_action, self.low_bound, self.high_bound)
 
         return sample_action
 
     def selected_logprob(self, actions):
-        logprob = -0.5 * tf.log(2 * np.pi)
-        logprob += -0.5 * tf.reduce_sum(self.logstd)
-        logprob += -0.5 * tf.reduce_sum(((actions - self.mean) / self.std) ** 2, axis=1)
+        logprob = -0.5 * tf.log(2 * np.pi) * self.num_actions
+        logprob += -0.5 * tf.reduce_sum(self.logstd, axis=-1)
+        logprob += -0.5 * tf.reduce_sum(((actions - self.mean) / self.std) ** 2, axis=-1)
 
         return logprob
 
@@ -36,13 +36,16 @@ class DiagGaussianDist:
         The terms are in order with the page
         '''
         assert isinstance(old_dist, DiagGaussianDist) and isinstance(new_dist, DiagGaussianDist)
-        first_term = tf.reduce_sum(tf.exp(old_dist.logstd - new_dist.logstd))
-        second_term = tf.reduce_sum(((new_dist.mean - old_dist.mean) ** 2)
-                                    / new_dist.std, axis=1)
-        third_term = tf.to_float(new_dist.num_actions)
-        fourth_term = tf.reduce_sum(new_dist.logstd) - tf.reduce_sum(old_dist.logstd)
+        # first_term = tf.reduce_sum(tf.exp(old_dist.logstd - new_dist.logstd))
+        # second_term = tf.reduce_sum(((new_dist.mean - old_dist.mean) ** 2)
+        #                             / new_dist.std, axis=1)
+        # third_term = tf.to_float(new_dist.num_actions)
+        # fourth_term = tf.reduce_sum(new_dist.logstd) - tf.reduce_sum(old_dist.logstd)
 
-        kl = 0.5 * (first_term + second_term - third_term + fourth_term)
-        kl = tf.reduce_mean(kl)
+        # kl = 0.5 * (first_term + second_term - third_term + fourth_term)
+        # kl = tf.reduce_mean(kl)
 
-        return kl
+        # return kl
+
+        return tf.reduce_sum(new_dist.logstd - old_dist.logstd + (tf.square(old_dist.std) + tf.square(old_dist.mean - new_dist.mean)) / (2.0 * tf.square(new_dist.std)) - 0.5, axis=-1)
+
