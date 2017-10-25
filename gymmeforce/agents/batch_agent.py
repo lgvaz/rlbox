f66aa714364e43bd808b27e7992e141936df050b
lgvaz <lucasgouvaz@gmail.com>
2017-10-24 23:43:19 -0200

    Added state scaler

--- gymmeforce/agents/batch_agent.py
+++ gymmeforce/agents/batch_agent.py
@@ -1,19 +1,25 @@
 import numpy as np
 from gymmeforce.agents import BaseAgent
+from gymmeforce.common.utils import Scaler
 
 
 class BatchAgent(BaseAgent):
     def __init__(self, env_name, normalize_advantages=True, **kwargs):
         super(BatchAgent, self).__init__(env_name, **kwargs)
+        self.scaler = Scaler(self.env_config['state_shape'])
 
     def _run_episode(self, env, render=False):
         state = env.reset()
         done = False
-        states, actions, rewards = [], [], []
+        states, actions, rewards, unscaled_states = [], [], [], []
+        scale, offset = self.scaler.get()
 
         while not done:
             if render:
                 env.render()
+            unscaled_states.append(state)
+            state = (state - offset) * scale
+            state = state
             states.append(state)
             # Select and execute action
             action = self.select_action(state)
@@ -25,6 +31,7 @@ class BatchAgent(BaseAgent):
             rewards.append(reward)
 
         trajectory = {'states': np.array(states),
+                      'unscaled_states': np.array(unscaled_states),
                       'actions': np.array(actions),
                       'rewards': np.array(rewards)}
 
@@ -38,5 +45,8 @@ class BatchAgent(BaseAgent):
             trajectory = self._run_episode(env)
             trajectories.append(trajectory)
             total_steps += trajectory['rewards'].shape[0]
+
+            unscaled = np.concatenate([traj['unscaled_states'] for traj in trajectories])
+            self.scaler.update(unscaled)
 
         return trajectories
--- gymmeforce/common/utils.py
+++ gymmeforce/common/utils.py
@@ -125,6 +125,55 @@ class ReplayBuffer:
         return np.array(start_idxs), np.array(end_idxs)
 
 
+class Scaler(object):
+    """
+    From: https://github.com/pat-coady/trpo/blob/master/src/utils.py#L13
+    Generate scale and offset based on running mean and stddev along axis=0
+    offset = running mean
+    scale = 1 / (stddev + 0.1) / 3 (i.e. 3x stddev = +/- 1.0)
+    """
+
+    def __init__(self, obs_dim):
+        """
+        Args:
+            obs_dim: dimension of axis=1
+        """
+        self.vars = np.zeros(obs_dim)
+        self.means = np.zeros(obs_dim)
+        self.m = 0
+        self.n = 0
+        self.first_pass = True
+
+    def update(self, x):
+        """ Update running mean and variance (this is an exact method)
+        Args:
+            x: NumPy array, shape = (N, obs_dim)
+        see: https://stats.stackexchange.com/questions/43159/how-to-calculate-pooled-
+               variance-of-two-groups-given-known-group-variances-mean
+        """
+        if self.first_pass:
+            self.means = np.mean(x, axis=0)
+            self.vars = np.var(x, axis=0)
+            self.m = x.shape[0]
+            self.first_pass = False
+        else:
+            n = x.shape[0]
+            new_data_var = np.var(x, axis=0)
+            new_data_mean = np.mean(x, axis=0)
+            new_data_mean_sq = np.square(new_data_mean)
+            new_means = ((self.means * self.m) + (new_data_mean * n)) / (self.m + n)
+            self.vars = (((self.m * (self.vars + np.square(self.means))) +
+                          (n * (new_data_var + new_data_mean_sq))) / (self.m + n) -
+                         np.square(new_means))
+            self.vars = np.maximum(0.0, self.vars)  # occasionally goes negative, clip
+            self.means = new_means
+            self.m += n
+
+    def get(self):
+        """ returns 2-tuple: (scale, offset) """
+        return 1/(np.sqrt(self.vars) + 0.1)/3, self.means
+
+
 def strided_axis0(a, L):
     '''
     https://stackoverflow.com/questions/43413582/selecting-multiple-slices-from-a-numpy-array-at-once/43413801#43413801
@@ -247,3 +296,7 @@ def tf_copy_params_op(from_scope, to_scope, soft_update=1.):
                  for from_scope_var, to_scope_var in zip(from_scope_vars, to_scope_vars)]
 
     return op_holder
+
+
+def explained_variance(y_true, y_pred):
+    return 1 - np.var(y_true - y_pred) / np.var(y_true)
